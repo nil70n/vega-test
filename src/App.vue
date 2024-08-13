f443f1878aed4ff12968fbe7dfa3d69cecdb5d20
Nilton Nascimento <nilton.nascimento@e-hps.com>
2024-08-13 16:36:58 -0300

    Add project files

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import { VegaButton } from "@heartlandone/vega-vue";
+
+export default {
+  components: { VegaButton },
+  methods: {
+    async notify() {
+      await window.VegaNotify.open({
+        duration: 240000,
+        message: `This notification will close after 4 minutes`,
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <VegaButton @vegaClick="notify">Click here!</VegaButton>
+</template>
